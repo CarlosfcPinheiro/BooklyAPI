446c4cfc09ce47e2f03b3f012a24e17002ef3ab1
Ítalo <163360684+iti0027@users.noreply.github.com>
2025-10-17 01:55:40 +0000

    criando a entidade gênero

---
+++ src/models/entities/gender.js
@@ -0,0 +1,25 @@
+const getGenderModel = (sequelize, {DataTypes}) => {
+    const gender = sequelize.define ("Gender", {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            unique: true,
+            allowNull: false,
+            primaryKey: true,
+            validate: {
+                notEmpty: true,
+            },
+        },
+        name: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+        },
+    });
+
+    gender.associate = (models) =>{
+        gender.hasMany (models.book, {onDelete: "CASCADE"});
+    };
+    return gender;
+};
+
+export default getGenderModel;
---
+++ src/models/entities/user.js
@@ -0,0 +1,6 @@
+// atributos para essa entidade
+/*
+id, name, email, password,registrationDate
+
+seguir padrão semelhante as entidades author, book, gender.
+*/
