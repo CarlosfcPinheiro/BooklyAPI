d395f1dac9a41e6c7d34048130d64f4ddcfb84ea
CarlosfcPinheiro <carlosfcpinheiro82@gmail.com>
2025-11-20 21:00:24 -0300

    feat: Adicionar proteção das rotas através do ownerShipMiddleware

--- src/routes/favorite.js
+++ src/routes/favorite.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import FavoriteController from '../controller/favorite.js';
+import authMiddleware from '../middleware/authMiddleware.js';
+import ownershipMiddleware from '../middleware/ownershipMiddleware.js';
 
 const router = express.Router();
 
@@ -7,9 +9,9 @@ router.get('/', FavoriteController.getAllFavorites);
 router.get('/:id', FavoriteController.getFavoriteById);
 router.get('/user/:userId', FavoriteController.getAllFavoritesByUserId);
 
-router.post('/', FavoriteController.createFavorite);
+router.post('/', authMiddleware, FavoriteController.createFavorite);
 
-router.delete('/:id', FavoriteController.deleteFavoriteById);
-router.delete('/', FavoriteController.deleteFavoriteByUserAndBook);
+router.delete('/:id', authMiddleware, ownershipMiddleware('favorite'), FavoriteController.deleteFavoriteById);
+router.delete('/', authMiddleware, FavoriteController.deleteFavoriteByUserAndBook);
 
 export default router;
